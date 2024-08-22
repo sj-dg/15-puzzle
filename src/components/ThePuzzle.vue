dbf640d738dd6d1b3bff4e07f8aeccdce5437c22
sj-dg <surajgjadhav1988+dg@gmail.com>
2024-08-22 12:40:20 +0530

    Use coordinates to check if tiles should move

--- src/components/ThePuzzle.vue
+++ src/components/ThePuzzle.vue
@@ -1,20 +1,39 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
 
+const MATRIX_SIZE = 4
+
 const tilesArr = ref([1, 2, 0, 4, 5, 6, 3, 8, 9, 10, 7, 12, 13, 14, 11, 15])
 
 const zeroTilePos = computed(() => tilesArr.value.findIndex((t) => t === 0))
 
+const coordinates = computed(() => {
+  return tilesArr.value.map((_, i) => {
+    return [Math.floor(i / MATRIX_SIZE), i % MATRIX_SIZE]
+  })
+})
+
 const shouldNotMove = (tileIndex: number) => {
-  if (tileIndex === 3 && zeroTilePos.value === 4) return true
-  if (tileIndex === 7 && zeroTilePos.value === 8) return true
-  if (tileIndex === 11 && zeroTilePos.value === 12) return true
+  const tileCoordinates = coordinates.value[tileIndex]
+  const zeroTileCoordinates = coordinates.value[zeroTilePos.value]
 
-  if (tileIndex === 4 && zeroTilePos.value === 3) return true
-  if (tileIndex === 8 && zeroTilePos.value === 7) return true
-  if (tileIndex === 12 && zeroTilePos.value === 11) return true
+  const xDiff = Math.abs(tileCoordinates[0] - zeroTileCoordinates[0])
+  const yDiff = Math.abs(tileCoordinates[1] - zeroTileCoordinates[1])
 
-  return false
+  return xDiff > 1 || yDiff > 1
+}
+
+const handleTitleClick = (tile: number, tileIndex: number) => {
+  if (tile === 0) return
+
+  const isMovingBack = tileIndex - 1 === zeroTilePos.value
+  const isMovingForward = tileIndex + 1 === zeroTilePos.value
+  const isMovingUp = tileIndex - MATRIX_SIZE === zeroTilePos.value
+  const isMovingDown = tileIndex + MATRIX_SIZE === zeroTilePos.value
+
+  if (isMovingBack || isMovingForward || isMovingUp || isMovingDown) moveZeroTile(tileIndex)
+
+  checkIfUserWon()
 }
 
 const moveZeroTile = async (tileIndex: number) => {
@@ -35,28 +54,14 @@ const checkIfUserWon = () => {
     alert('Bingo!')
   }
 }
-
-const handleTitleClick = (tile: number, tileIndex: number) => {
-  if (tile === 0) return
-
-  if (
-    tileIndex - 1 === zeroTilePos.value ||
-    tileIndex + 1 === zeroTilePos.value ||
-    tileIndex + 4 === zeroTilePos.value ||
-    tileIndex - 4 === zeroTilePos.value
-  )
-    moveZeroTile(tileIndex)
-
-  checkIfUserWon()
-}
 </script>
 
 <template>
-  <div class="grid grid-cols-4 gap-1 max-w-96 mt-16 mx-auto">
+  <div class="grid grid-cols-4 gap-1 mt-16 max-w-96 mx-auto">
     <div
       v-for="(tile, index) in tilesArr"
       :key="index"
-      class="bg-yellow-200 p-4 text-center text-xl rounded-md border border-blue-600 cursor-pointer"
+      class="bg-yellow-200 py-6 px-4 text-center text-xl rounded-md border border-blue-600 cursor-pointer"
       :class="{ invisible: tile === 0 }"
       role="button"
       @click="handleTitleClick(tile, index)"
